c20599c67c51f22a67c08998f0b934fa680442b7
Palaniappan <Somu.Palaniappan@McKesson.com>
2019-02-13 11:56:52 -0600

    done

--- main.py
+++ main.py
@@ -5,3 +5,26 @@
 # that new vector
 # find the norm of the final vector
 # also find the dot product between the 2nd and 3rd vector
+
+from vector import Vector
+
+with open('vectors.txt') as f:
+    vec_list = []
+    for line in f:
+        vec = list(map(int, line.strip().split(',')))
+        vec_list.append(Vector(*vec))
+
+vec1_scale3 = vec_list[0].scale(3)
+vec_last_scale3 = vec_list[-1].scale(3)
+
+vec_list.append(vec1_scale3)
+vec_list.append(vec_last_scale3)
+
+new_vec = vec_list[0]
+
+for i in range(1, len(vec_list)):
+    new_vec = new_vec + vec_list[i]
+
+final_norm = new_vec.length()
+print(final_norm)
+print(vec_list[1].dot(vec_list[2]))
--- vector.py
+++ vector.py
@@ -6,3 +6,50 @@
 # norm or len
 # dot product
 # override __str__ and __repr__
+
+
+class Vector:
+
+    def __init__(self, *args):
+        self.v = args
+        self.len = len(args)
+
+    def __str__(self):
+        return "vector is {} and dimension is {}".format(self.v, self.len)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def scale(self, scale):
+        scale_vec = []
+        for v in self.v:
+            scale_vec.append(v * scale)
+        return Vector(*scale_vec)
+
+    def __add__(self, vec):
+        add_vec = []
+        for i in range(len(self.v)):
+            add_vec.append(self.v[i] + vec.v[i])
+        return Vector(*add_vec)
+
+    def length(self):
+        ans = 0
+        for n in self.v:
+            ans = ans + n**2
+        return ans**(1 / 2)
+
+    def dot(self, vec):
+        dot_prod = 0
+        for i in range(len(self.v)):
+            dot_prod += self.v[i] * vec.v[i]
+        return dot_prod
+
+
+# a = Vector(2, 3)
+# print(a)
+# b = a.scale(2)
+# print(b)
+# c = a + b
+# print(c)
+# print(a.length())
+# print(a.dot(b))
