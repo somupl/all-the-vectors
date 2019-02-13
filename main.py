0edb94b7daa1e67562ca85b59710dd417b710a77
Chyld Medford <chyld.medford@gmail.com>
2019-02-13 08:41:38 -0800

    instructions

---
+++ main.py
@@ -0,0 +1,7 @@
+# read in the vectors.txt file
+# create a  list of vectors
+# scale the first and last vector by 3
+# add them all together and find
+# that new vector
+# find the norm of the final vector
+# also find the dot product between the 2nd and 3rd vector
---
+++ vector.py
@@ -0,0 +1,8 @@
+# instructions
+# create a vector class
+# that should...
+# add vectors
+# scale vector
+# norm or len
+# dot product
+# override __str__ and __repr__
